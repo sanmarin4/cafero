9b06809deb776a46db722dfa5938092cae7c9343
John Angelo David <johnangelo.programming@gmail.com>
2025-09-26 19:21:46 +0800

    Type your message here

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -232,7 +232,7 @@ const AdminDashboard: React.FC = () => {
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    if (password === 'ClickEats@Admin!2025') {
+    if (password === 'BlueprintCafe@Admin!2025') {
       setIsAuthenticated(true);
       localStorage.setItem('beracah_admin_auth', 'true');
       setLoginError('');
